6ff29124cc346f27f7ba790cd1f11bd0e14f0f39
ThiagoEvan <Tevangelista504@gmail.com>
2022-11-18 17:48:55 -0300

    corrigindo erros com as importações

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Post } from "./components/Post"
 import { Header} from './components/Header'
+import { Sidebar } from "./components/Sidebar"
+
 import './global.css'
 
 import styles from './App.module.css'
-import { Sidebar } from "./components/Sidebar"
+
 
 export function App() {
   return (
     <div className={styles.wrapper}>
        <Header />
        <Sidebar />
+       
        <Post author="Alef" content="Professor de DS44"/>
        <Post author="Eduardo" content="Estudante de React" />
        <Post author="Rafael" content="To tentando entender" />
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,8 @@ import styles from './Post.module.css'
 export function Post(props){
     return (
         <div className={styles.post}>
-            <h1>{ props.author }</h1>
-            <h2>{ props.content }</h2>
+            <h1 className={styles.author}>{ props.author }</h1>
+            <h2 className={styles.content}>{ props.content }</h2>
         </div>
     )
 }
