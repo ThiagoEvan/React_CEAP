d956b53564f17cb3b1b717e5b673e0d019f9ee45
ThiagoEvan <Tevangelista504@gmail.com>
2022-11-18 15:58:01 -0300

    module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { Post } from "./Post"
-import { Header } from "./Header"
-import { Sliderbar } from "./Sliderbar"
+import { Post } from "./components/Post"
+import { Header} from './components/Header'
 import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
-    <>
-       <Header/>
-       <Sliderbar/>
+    <div className={styles.wrapper}>
+       <Header />
+       <Sidebar />
        <Post author="Alef" content="Professor de DS44"/>
        <Post author="Eduardo" content="Estudante de React" />
        <Post author="Rafael" content="To tentando entender" />
 
 
-    </>
+    </div>
   )
 }
 
